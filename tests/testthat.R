d193f27cddf3e739a38efa6de45eb7ddf2e5fe95
daviel9 <Luke.Davies@ons.gov.uk>
2026-07-16 14:22:31 +0100

    Update quarto pages

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(CARS)
-
-test_check("CARS")
--- tests/testthat/test-apply_skip_logic.R
+++
@@ -1,9 +0,0 @@
-test_that("apply_skip_logic replaces all relevant columns with NAs", {
-  dummy_data <- rename_cols(cars_dummy_data_test, config)
-
-  got <- apply_skip_logic(dummy_data, config)
-
-  expected <- rename_cols(cars_dummy_data_clean, config)
-
-  expect_equal(expected, got)
-})
